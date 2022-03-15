8f494f09ed2f0810715f136937bd115dd0a826b2
salties <yanyansmajesty@outlook.com>
2022-03-15 15:25:26 +0100

    Smurf option fix.

--- Linux/uelmo/uelmo.h
+++ Linux/uelmo/uelmo.h
@@ -2,10 +2,12 @@
 #define UELMO_H
 #define UELMO_ERROR (-1)
 
+#ifdef USE_SMURF
 #include "smurf/smurf.h"
 #include "smurf/emulator.h"
 
 #define ELMO_IO_PATH "/tmp/elmoio"
 SmurfIO *sio;
+#endif
 
 #endif
