67e4972bdf7516a897772972c0777cd5f941ca78
Salties <yanyansmajesty@outlook.com>
2023-06-23 18:12:15 +0200

    Ongoing implementation.

--- Linux/uelmo/ulang.c
+++ Linux/uelmo/ulang.c
@@ -1,5 +1,3 @@
-#ifdef USE_SMURF
-
 #include <stdio.h>		//DEBUG ONLY
 
 #include "smurf/smurf.h"
@@ -31,7 +29,18 @@ int InitScript(const char *scriptpath)
     //Enable skip indent.
     script->skipindent = true;
 
+    //Initialise the supported ULANG commands.
+    InitUlangCmd();
+
     return 0;
+}
+
+//Clean resources related to the script.
+void CleanScript()
+{
+    CleanUlangCmd();
+    FreeScript(script);
+    return;
 }
 
 //Check if a char is in the blank set.
@@ -109,17 +118,57 @@ static int IsUlangCmd(const char *line)
 //Process a line in a command block.
 void UlangSed(int linenumber, const char *line, void *arg)
 {
-    const char *nextarg;
+    char *nextarg = NULL;
+    int arglen = 0;
+
+    //argc and argv for ULANG commands.
+    char *argline = NULL;
+    int uargc = 0;
+    char *uargv[ULANG_MAX_ARGC] = { 0 };
 
     //Check if the line is a Ulang command.
     //Ignore all non command lines.
     if (IsUlangCmd(line))
 	{
 	    //Process the command.
-
 	    //DBG
 	    printf("%d(%d) - %d\t: %s\n", cyclecount, frameno, linenumber,
 		   line);
+
+	    //Skip the first ULANG notifier.
+	    nextarg = (char *)GetNextArg(line, &arglen);
+	    nextarg += arglen;
+
+	    //Create editable copy excluding the notifier.
+	    argline = CloneString(nextarg);
+	    nextarg = argline;
+
+	    //Construct argc and argv.
+	    while (ULANG_MAX_ARGC > uargc)
+		{
+		    if (NULL == (nextarg = (char *)GetNextArg(nextarg, &arglen)))	//All args processed.
+			{
+			    break;
+			}
+
+		    //Parse into argv.
+		    uargv[uargc] = nextarg;
+		    uargc++;
+
+		    //Advance to the nextarg.
+		    nextarg += arglen;
+
+		    //Swap ' ' bt '\0'
+		    if (IsBlank(*nextarg))
+			{
+			    *nextarg = '\0';
+			    nextarg++;
+			}
+		}
+
+	    //Execute the command.
+	    UlangCbManager(uargv[0], uargc, uargv);
+	    Free(argline);
 	}
 
     return;
@@ -132,4 +181,74 @@ int ReadNextCmdBlock()
     return 0;
 }
 
-#endif
+//ULANG callback manager. argc and argv are passed to the handler functions.
+void UlangCbManager(const char *op, int argc, char **argv)
+{
+    //Match instructions.
+    if (!strncmp(op, ULANG_SRC, sizeof(ULANG_SRC)))	//src
+	{
+	    RequestSrc(argc, argv);
+	}
+    else if (!strncmp(op, ULANG_DST, sizeof(ULANG_DST)))	//dst
+	{
+	    RequestDst(argc, argv);
+	}
+    else			//Unknown instruction.
+	{
+	    printf("ERROR: unknown command: %s\n", op);
+	    INFO("Not supported ULANG command:");
+	    INFO(op);
+	    INFO("\n");
+	}
+
+    return;
+}
+
+//ULANG commands.
+int InitUlangCmd()
+{
+    srcqueue = NewSmurfQueue();
+    dstqueue = NewSmurfQueue();
+    return 0;
+}
+
+void CleanUlangCmd()
+{
+    DelSmurfQueue(dstqueue, NULL);
+    DelSmurfQueue(srcqueue, NULL);
+    return;
+}
+
+//Handler: src REG SYMBOL
+SmurfQueue *srcqueue = NULL;
+void RequestSrc(int argc, char **argv)
+{
+    int reg = 0;
+
+    //Scan command arguments for parameters.
+    if (!sscanf(argv[1], "r%d", &reg))
+	{
+	    printf("#Unknown parameter: %s\n", argv[1]);
+	    return;
+	}
+    printf("Request src: REG=%d\n", reg);
+
+    return;
+}
+
+//Handler: src REG SYMBOL
+SmurfQueue *dstqueue = NULL;
+void RequestDst(int argc, char **argv)
+{
+    int reg = 0;
+
+    //Scan command arguments for parameters.
+    if (!sscanf(argv[1], "r%d", &reg))
+	{
+	    printf("#Unknown parameter: %s\n", argv[1]);
+	    return;
+	}
+    printf("Request dst: REG=%d\n", reg);
+
+    return;
+}
