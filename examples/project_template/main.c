e7f19f751471851bb4c14d3bfff0d924248eb7ca
Salties <yanyansmajesty@outlook.com>
2025-01-14 12:38:01 +0100

    Template update.

--- examples/project_template/main.c
+++ examples/project_template/main.c
@@ -3,7 +3,27 @@
 #include "dep1.h"
 #include "dep2.h"
 
-const char hellostr[] = "hello,world!\n";
+//Nibble to hex.
+static char ntohex(int nib)
+{
+    if(0xA > nib)
+    {
+        return '0' + nib;
+    }
+    else
+    {
+        return 'A' + nib;
+    }
+    return 0;
+}
+
+//uint8 to hex.
+static void u8tohex(uint8_t in, char *out)
+{
+    out[0] = ntoh(in >> 4);
+    out[1] = ntoh(in & 0x0F);
+    return;
+}
 
 static void Print(const char *str, int len)
 {
@@ -16,16 +36,28 @@ static void Print(const char *str, int len)
     return;
 }
 
+//Print a char array.
+const char hellostr[] = "hello,world!\n";
+
 int main(void)
 {
-    int x = 0, y = 0;
+    uint8_t x = 0, y = 0;
+    char str[2] = { 0 };
 
     Print(hellostr, sizeof(hellostr));
 
-    starttrigger();
+    starttrigger();             //Trace start.
     DepFunc1(x, y);
     DepFunc2(x, y);
-    endtrigger();
+    endtrigger();               //Trace end.
+
+    u8tohex(x, str);
+    Print(str, sizeof(str));
+    Print("\n", 1);
+
+    u8tohex(y, str);
+    Print(str, sizeof(str));
+    Print("\n", 1);
 
     endprogram();
     return 0;
