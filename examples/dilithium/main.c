f3696adb57dc82dd8335988cecef3b685a0bfedf
salties <yanyansmajesty@outlook.com>
2025-02-19 18:32:47 +0100

    Option to read input from GPIO.

--- examples/dilithium/main.c
+++ examples/dilithium/main.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
+#include <stdbool.h>
 #include "config.h"
 #include "ntt.h"
 
+#define RAND_MTX ((0))
+
 const char hellostr[] = "hello,world!\n";
 
-//Nibble to hex.
 static void PrintNttArray(int32_t *nttarray)
 {
     int i;
@@ -19,6 +21,7 @@ static void PrintNttArray(int32_t *nttarray)
     return;
 }
 
+#if RAND_MTX
 static void Randint32(int32_t *rint)
 {
     int i;
@@ -35,28 +38,59 @@ static void Randint32(int32_t *rint)
 
     return;
 }
+#else
+static int32_t ReadInt(void)
+{
+    int32_t ri = 0;
+    int nshift = 24;
+
+    //Read from big endian and store in little endian.
+    for (int i = 0; i < 4; i++, nshift -= 8)
+    {
+        unsigned char b = 0;
+        readbyte(&b);
+        ri ^= (b << nshift);
+    }
+
+    return ri;
+}
+
+static void ReadMtx(int32_t *nttarray)
+{
+    for (int i = 0; i < N; i++)
+    {
+        nttarray[i] = ReadInt();
+    }
+    return;
+}
+#endif
 
 int main(void)
 {
-    int i;
     int32_t nttarray[N] = { 0 };
-    int32_t r = 0;
 
     PrintStr(hellostr, sizeof(hellostr));
 
-    //Randomise input.
-    for (i = 0; i < N; i++)
+    while (true)
     {
-        Randint32(&r);
-        nttarray[i] = r;
+#if RAND_MTX
+        //Randomise NTT input matrix.
+        for (int i = 0; i < N; i++)
+        {
+            Randint32(&nttarray[i]);
+        }
+#else
+        //Read NTT input matrix from GPIO.
+        ReadMtx(nttarray);
+#endif
+        PrintNttArray(nttarray);
+
+        starttrigger();         //Trace start.
+        ntt(nttarray);
+        endtrigger();           //Trace end.
+
+        PrintNttArray(nttarray);
     }
-    PrintNttArray(nttarray);
-
-    starttrigger();             //Trace start.
-    ntt(nttarray);
-    endtrigger();               //Trace end.
-
-    PrintNttArray(nttarray);
 
     endprogram();
     return 0;
