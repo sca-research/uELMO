38faa5de7d5daf710f9e38c1d742eec370d9c292
salties <yanyansmajesty@outlook.com>
2025-04-07 19:40:35 +0200

    Style update

--- examples/helloworld/elmoasmfunctionsdef.h
+++ examples/helloworld/elmoasmfunctionsdef.h
@@ -42,14 +42,14 @@
 
 extern void starttrigger(void);
 extern void endtrigger(void);
-extern void randbyte(unsigned char * pointer);
-extern void LoadN(void* addr);
-extern void readbyte(unsigned char * pointer);
-extern void printbyte(const unsigned char * pointer);
+extern void randbyte(unsigned char *pointer);
+extern void LoadN(void *addr);
+extern void readbyte(unsigned char *pointer);
+extern void printbyte(const unsigned char *pointer);
 extern void endprogram(void);
-extern void getstart(unsigned int * pointer);
-extern void getruncount(unsigned int * pointer);
-extern void initialisemaskflow(unsigned char * pointer);
+extern void getstart(unsigned int *pointer);
+extern void getruncount(unsigned int *pointer);
+extern void initialisemaskflow(unsigned char *pointer);
 extern void resetmaskflow(void);
 extern void setmaskflowstart(unsigned int start);
 extern void resetdatafile(void);
