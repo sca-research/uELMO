fe8fc2f8cea33863ceb330e2080c325d04092322
salties <yanyansmajesty@outlook.com>
2025-04-07 19:47:07 +0200

    Update

--- examples/helloworld/helloworld.c
+++ examples/helloworld/helloworld.c
@@ -1,9 +1,11 @@
 #include "helloworld.h"
+#include <string.h>
+#include <stdbool.h>
 
 #define MAX_LINE_LEN ((256))
-#define TERMINATOR (('\0'))
+#define TERMINATOR (("exit\n"))
 
-static int ReadLine(unsigned char *line, int len)
+static int ReadLine(char *line, int len)
 {
     int i = 0;
     unsigned char c = 0;
@@ -17,6 +19,7 @@ static int ReadLine(unsigned char *line, int len)
         //EOL
         if(('\n' == c) || ('\0' == c) || (0x04 == c))
         {
+            i++;
             break;
         }
     }
@@ -24,13 +27,17 @@ static int ReadLine(unsigned char *line, int len)
     return i;
 }
 
-static void PrintLine(unsigned char *line, int len)
+static void PrintLine(char *line, int len)
 {
     int i = 0;
 
     for (i = 0; i < len; i++)
     {
-        printbyte(&line[i]);
+        printbyte((uint8_t *) & line[i]);
+        if(line[i] == '\0' || line[i] == '\n')
+        {
+            break;
+        }
     }
 
     return;
@@ -38,25 +45,39 @@ static void PrintLine(unsigned char *line, int len)
 
 int main(void)
 {
-    const unsigned char helloworld[] = "hello,world\n";
-    unsigned char line[MAX_LINE_LEN] = { 0 };
+    int nread = 0;
+    char line[MAX_LINE_LEN] = { 0 };
+
+    PrintLine("hello,world\n", MAX_LINE_LEN);
 
     starttrigger();
 
-    //Helloworld.
-    for (int i = 0; i < (int)sizeof(helloworld); i++)
-    {
-        printbyte(&helloworld[i]);
-    }
-
     //Echo server.
-    while (0 != ReadLine(line, MAX_LINE_LEN - 1))
+    while (true)
     {
-        if('\0' == line[0] || '\n' == line[0])
+        memset(line, 0, sizeof(line));
+        nread = ReadLine(line, MAX_LINE_LEN - 1);
+        if(0 == nread)
         {
+            //Connection broke.
+            continue;
+        }
+
+        //Control commands.
+        if(!strncmp(line, "\n", 1))
+        {
+            PrintLine("ACK\n", MAX_LINE_LEN);
+            continue;
+        }
+        else if(!strncmp(line, TERMINATOR, strlen(TERMINATOR)))
+        {
+            PrintLine("Server closed.\n", MAX_LINE_LEN);
             break;
         }
-        PrintLine(line, MAX_LINE_LEN);
+        else
+        {
+            PrintLine(line, MAX_LINE_LEN);
+        }
     }
 
     endtrigger();
