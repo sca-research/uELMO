31cde5e92d55dd0a23af1fb4785ed6588a40a725
Yan Yan <IWAS\yanyan@valentia.aics.aau.at>
2025-04-11 18:10:01 +0200

    Quick script to run TVLA on TVLA_MaskedAES

---
+++ runtvla.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+N=100
+PORT="/tmp/uelmoport"
+WAIT="0.1"
+OUTPUT="/tmp/tvla/"
+BIN="examples/TVLA_MaskedAES/MaskedAesFull.bin"
+FIXED_CLIENT="python3 examples/TVLA_MaskedAES/client.py -e -w ${WAIT} -n ${N} -p ${PORT}"
+RANDOM_CLIENT="python3 examples/TVLA_MaskedAES/client.py -e -w ${WAIT} -r -n ${N} -p ${PORT}"
+EXTRACTOR="LeakageExtractor/elmo.py"
+
+python3 tvla.py ${BIN} -p ${PORT} \
+    -f ' '"${FIXED_CLIENT}"' ' \
+    -r ' '"${RANDOM_CLIENT}"' ' \
+    -le ${EXTRACTOR} \
+    -d ${OUTPUT}
